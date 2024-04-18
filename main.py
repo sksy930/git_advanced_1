c95fd23219a7b12a147ab95a56a323414f6ef2a0
sksy930 <sy951020@g.skku.edu>
2024-04-18 21:40:53 +0900

    main initialized

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import List
+def even_list(int_list: List[int]) -> List[int]:
+    # TODO: Implement even_list  
+    pass
+# Skeleton code for sum_of_squares_of_even
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    # TODO: Implement sum_of_squares_of_even
+    pass
+# Main function
+def main():
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+# Boilerplate code
+if __name__ == "__main__":
+    main()
